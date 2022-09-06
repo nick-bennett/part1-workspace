5a165e686836131efa927d1a5a6d91eb89a5e4f2
nick-bennett <nick@nickbenn.com>
2022-09-06 11:55:59 -0600

    Added verifyInternetConnection.

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -1,29 +1,40 @@
 public class Television {
 
-    String brand;
-    int volume;
+  private String brand;
+  private int volume;
 
-    public void turnOn() {
-        System.out.println("The " + brand + " television is on, with a volume of " + volume);
-    }
+  public void turnOn() {
+    boolean isConnected = verifyInternetConnection();
+    System.out.println("The " + brand + " television is on, with a volume of " + volume);
+  }
 
-    public void turnOff() {
-        System.out.println("The " + brand + " television is off");
-    }
+  public void turnOff() {
+    System.out.println("The " + brand + " television is off");
+  }
 
-    public String getBrand() {
-        return brand;
-    }
+  public String getBrand() {
+    return brand;
+  }
 
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
 
-    public int getVolume() {
-        return volume;
-    }
+  public int getVolume() {
+    return volume;
+  }
 
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
+  public void setVolume(int volume) {
+    this.volume = volume;
+  }
+
+  private boolean verifyInternetConnection() {
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "Television{brand='" + brand + "', volume=" + volume + "}";
+  }
+
 }
--- Lab02.2/src/TelevisionClient.java
+++ Lab02.2/src/TelevisionClient.java
@@ -3,12 +3,12 @@ public class TelevisionClient {
     public static void main(String[] args) {
 
         Television tv1 = new Television();
-        tv1.brand = "Zenith";
-        tv1.volume = 25;
+        tv1.setBrand("Zenith");
+        tv1.setVolume(25);
 
         Television tv2 = new Television();
-        tv2.brand = "RCA";
-        tv2.volume = 10;
+        tv2.setBrand("RCA");
+        tv2.setVolume(10);
 
         tv1.turnOn();
         tv1.turnOff();
