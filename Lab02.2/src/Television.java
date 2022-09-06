91539e1a32effe984f10059727fd7340efcc968e
nick-bennett <nick@nickbenn.com>
2022-09-06 10:15:20 -0600

    Adding some ignored files.

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -3,12 +3,27 @@ public class Television {
     String brand;
     int volume;
 
-    void turnOn() {
+    public void turnOn() {
         System.out.println("The " + brand + " television is on, with a volume of " + volume);
     }
 
-    void turnOff() {
+    public void turnOff() {
         System.out.println("The " + brand + " television is off");
     }
 
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 }
